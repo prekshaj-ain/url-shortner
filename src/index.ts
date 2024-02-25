1ce58112f2cb52b42fd67b1a5d1786b3f30172c5
preksha <jainpreksha2712@gmail.com>
2024-02-25 20:24:43 +0530

    connect database

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
+import mongoose from "mongoose";
 import app from "./app";
+import dotenv from "dotenv";
 
-app.listen(8000, () => {
-  console.log("server running at 8000");
-});
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection FAILED ", error);
+    process.exit(1);
+  }
+};
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
